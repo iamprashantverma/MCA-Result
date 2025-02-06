991e3d6bd025603dc43752577ea18b6c8262e6cf
iamprashantverma <prashant2k26@gmail.com>
2025-02-06 11:53:56 +0530

    controller method created

--- src/main/java/com/college/mca/result/controller/ResultController.java
+++ src/main/java/com/college/mca/result/controller/ResultController.java
@@ -1,7 +1,26 @@
 package com.college.mca.result.controller;
 
+import com.college.mca.result.dto.ResultDTO;
+import com.college.mca.result.service.ResultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class ResultController {
+
+    private final ResultService resultService;
+
+    @GetMapping("/junior")
+    public ResponseEntity<List<ResultDTO>> getJuniorResult() {
+       List<ResultDTO> results = resultService.getJuniorsResult();
+       return  ResponseEntity.ok(results);
+    }
+
 }
