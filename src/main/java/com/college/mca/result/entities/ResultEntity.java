54b653e0dd9daebf9dca5a3ed0a9f3203d4a134d
iamprashantverma <prashant2k26@gmail.com>
2025-02-06 11:39:16 +0530

    entity created

--- src/main/java/com/college/mca/result/entities/ResultEntity.java
+++ src/main/java/com/college/mca/result/entities/ResultEntity.java
@@ -1,9 +1,11 @@
 package com.college.mca.result.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "result")
-public class Result {
+public class ResultEntity {
+
 }
