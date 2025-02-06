903c3555c43d0e165f5c5312f7c8ea7c65fdaa5e
Shailendra <shailendrarai855@gmail.com>
2025-02-06 11:54:09 +0530

    entity dto edit

--- src/main/java/com/college/mca/result/entities/ResultEntity.java
+++ src/main/java/com/college/mca/result/entities/ResultEntity.java
@@ -1,11 +1,24 @@
 package com.college.mca.result.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "result")
 public class ResultEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String schNo;
+
+    private String name;
+
+    private String sem;
+
+    private Long sgpa;
+
+    private Long cgpa;
+
 
 }
