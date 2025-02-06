1ab2c5ee5a96ba447056a1da79fbb56aafb90fa4
iamprashantverma <prashant2k26@gmail.com>
2025-02-06 11:30:45 +0530

    app created

---
+++ src/main/java/com/college/mca/result/McaResultApplication.java
@@ -0,0 +1,13 @@
+package com.college.mca.result;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McaResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McaResultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/mca/result/McaResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.mca.result;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McaResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
